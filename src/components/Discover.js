a83632e31fc70d2cc921f693f1bfcb7200c9ff6f
TRNisbet <TRNisbet@hotmail.com>
2021-02-15 18:27:22 -0500

    sort function working on first name

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -2,14 +2,22 @@ import React, { Component } from "react";
 import API from "../utils/API";
 
 export default class Discover extends Component {
-    state = {
+   
+   constructor(props) {
+       super(props);
+
+    this.state = {
         Employees: [],
         sorted: false,
     };
+       this.handleOnClick = this.handleOnClick.bind(this);
+    }
 
     componentDidMount() {
         this.loadNextEmployees();
     }
+
+
 
     loadNextEmployees = () => {
         API.getEmployees()
@@ -21,6 +29,30 @@ export default class Discover extends Component {
             .catch(err => console.log(err));
     };
 
+    handleOnClick = (e) => {
+        function compare(a, b) {
+            const emp1 = a.name.first.toUpperCase();
+            const emp2 = b.name.first.toUpperCase();
+
+            if (emp1<emp2) {
+                return -1;
+            }
+            if (emp1>emp2) {
+                return 1;
+            }
+            // a must be equal to b
+            return 0;
+        }
+
+var sortedEmp = this.state.Employees.sort(compare);
+
+            this.setState({
+                   Employees: sortedEmp
+                })
+           
+
+    }
+
     render() {
         return (
             <div>
@@ -28,17 +60,21 @@ export default class Discover extends Component {
                    <caption className="text-center">Employee Directory</caption>
                    <thead>
                        <tr> 
-                           <th> First Name</th>
-                            <th> Last Name</th>
-                            <th> Email</th>
+                            <th onClick={this.handleOnClick}>First Name</th>
+                            <th>Last Name</th>
+                            <th>Email</th>
+                            <th>UUID</th>
+                            <th>Gender</th>
                         </tr>
                    </thead>
                        <tbody>
                     {this.state.Employees.map((Employee) => {
-                        return (<tr>
+                        return (<tr key={Employee.login.uuid}>
                             <td>{Employee.name.first} </td>
                             <td>{Employee.name.last} </td>
                             <td>{Employee.email} </td>
+                            <td>{Employee.login.uuid} </td>
+                            <td>{Employee.gender} </td>
                         </tr>
                     )})
                 }
